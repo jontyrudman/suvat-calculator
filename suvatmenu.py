2c34075a9ee868128103fdca17b753fcc7cef900
JontySR <jonathan.rudman@live.co.uk>
2017-03-04 18:49:03 +0000

    Soz github I failed.

--- suvatmenu.py
+++ suvatmenu.py
@@ -14,35 +14,28 @@ while True:
         sx = input("x: range = ")
         ux = input("   initial velocity = ")
 
-        # If there is no value for initial velocity (ux), ask for vx
-            # and set ux to vx.
-        if "" == ux:
-            vx = input("   final velocity = ")
-            if "" not in vx:
-                ux = vx
-        # Otherwise, set vx to ux.
-        else:
-            vx = ux
-            print("   final velocity =", vx)
+        vx = input("   final velocity = ")
 
-        # Notifies the user that x accel is 0.
-        print("   acceleration = 0.0")
+        ax = input("   acceleration = ")
 
         # Asks for total time.
         tx = input("   total time = ")
 
         # Asks for the y values.
         sy = input("\ny: height at apex = ")
+
         uy = input("   initial velocity = ")
 
         # Notifies the user that the velocity 
             # at the apex will always be 0.
         print("   velocity at apex = 0.0")
+        vy = "0"
+
         ay = input("   acceleration = ")
 
         # If there is no value for total time (tx), ask for ty
             # and set tx as 2*ty.
-        if "" == tx:
+        if "" in tx:
             ty = input("   time to reach apex = ")
             if "" not in ty:
                 tx = 2*float(ty)
@@ -51,9 +44,9 @@ while True:
             ty = float(tx)/2
             print("   time to reach apex =", ty)
 
-        # If menuvalidator() returns True, tell the user their input
+        # If menuvalidator() returns False, tell the user their input
             # configuration is invalid.
-        if menuvalidator(sx, ux, vx, tx, sy, uy, ay, ty) == False:
+        if menuvalidator(sx, sy, ux, uy, vx, vy, ax, ay, tx, ty) == False:
              print("\n"*10, "Invalid. Got any more values? Trying again...\n")
              continue
         # Otherwise, break out of the loop and finish.
--- validator.py
+++ validator.py
@@ -3,7 +3,7 @@ import math
 
 # Validates the values inputted by sending them to
     # the correct function and returning True.
-def menuvalidator(sx, ux, vx, tx, sy, uy, ay, ty):
+def menuvalidator(sx, sy, ux, uy, vx, vy, ax, ay, tx, ty):
     
     # If ux, uy and ay have values, send them to
         # functions.uxuyay() and return True.
@@ -20,78 +20,163 @@ def menuvalidator(sx, ux, vx, tx, sy, uy, ay, ty):
     conditions_unmet = 0
     while True:
 
-        if tx == "":
-            if "" not in (sx, ux, vx):
-                tx = functions.tx_equals_2sx_over_ux_plus_vx(sx, ux, vx)
-                ty = float(tx)/2
-
-            elif "" not in (uy, ay):
-                ty = functions.ty_equals_minus_uy_over_ay(uy, ay)
-                tx = 2*float(ty)
-
-            elif "" not in (sy, uy):
-                ty = functions.ty_equals_2_times_sy_over_uy(sy, uy)
-                tx = 2*float(ty)
-
-            elif "" not in (sx, ux):
-                tx = functions.tx_equals_sx_over_ux(sx, ux)
-                ty = float(tx)/2
-
-            else: conditions_unmet += 1
-
-        if uy == "":
-            if "" not in (ay, sy):
-                uy = functions.uy_equals_sqrt_vy_squared_minus_2aysy(ay, sy)
-
-            elif "" not in (ay, ty):
-                uy = functions.uy_equals_vy_minus_ay_times_ty(ay, ty)
-
-            elif "" not in (sy, ay, ty):
-                uy = functions.uy_equals_sy_minus_half_ay_ty_sq_over_ty(sy, ay, ty)
-
-            elif "" not in (sy, ty):
-                uy = functions.uy_equals_2sy_over_ty_minus_vy(sy, ty)
-
-            else: conditions_unmet += 1
-
         if sx == "":
-            if "" not in (ux, tx):
-                sx = functions.sx_equals_ux_times_tx(ux, tx)
+
+            if "" not in (ux, ax, tx):
+                sx = functions.s_uat(ux, ax, tx)
+
+            elif "" not in (vx, ax, tx):
+                sx = functions.s_vat(vx, ax, tx)
+
+            elif "" not in (ux, vx, ax):
+                sx = functions.s_uva(ux, vx, ax)
+
+            elif "" not in (ux, vx, tx):
+                sx = functions.s_uvt(ux, vx, tx)
 
             else: conditions_unmet += 1
 
         if sy == "":
-            if "" not in (uy, ay):
-                sy = functions.sy_equals_minus_uy_squared_over_2_times_ay(
-                        uy, ay)
-            elif "" not in (uy, ty, ay):
-                sy = functions.sy_equals_ut_plus_half_a_times_t_squared(
-                        uy, ty, ay)
-            elif "" not in (ty, ay):
-                sy = functions.sy_equals_vt_minus_half_a_times_t_squared(
-                        0, ty, ay)
+
+            if "" not in (uy, ay, ty):
+                sy = functions.s_uat(uy, ay, ty)
+
+            elif "" not in (vy, ay, ty):
+                sy = functions.s_vat(vy, ay, ty)
+
+            elif "" not in (uy, vy, ay):
+                sy = functions.s_uva(uy, vy, ay)
+
+            elif "" not in (uy, vy, ty):
+                sy = functions.s_uvt(uy, vy, ty)
 
             else: conditions_unmet += 1
 
         if ux == "":
-            if "" not in (sx, tx):
-                ux = functions.ux_equals_sx_over_tx(sx, tx)
-                vx = ux
+
+            if "" not in (vx, ax, tx):
+                ux = functions.u_vat(vx, ax, tx)
+
+            elif "" not in (sx, ax, tx):
+                ux = functions.u_sat(sx, ax, tx)
+
+            elif "" not in (sx, vx, ax):
+                ux = functions.u_sva(sx, vx, ax)
+
+            elif "" not in (sx, vx, tx):
+                ux = functions.u_svt(sx, vx, tx)
+
+            else: conditions_unmet += 1
+
+        if uy == "":
+
+            if "" not in (vy, ay, ty):
+                uy = functions.u_vat(vy, ay, ty)
+
+            elif "" not in (sy, ay, ty):
+                uy = functions.u_sat(sy, ay, ty)
+
+            elif "" not in (sy, vy, ay):
+                uy = functions.u_sva(sy, vy, ay)
+
+            elif "" not in (sy, vy, ty):
+                uy = functions.u_svt(sy, vy, ty)
+
+            else: conditions_unmet += 1
+
+        if vx == "":
+
+            if "" not in (ux, ax, tx):
+                vx = functions.v_uat(ux, ax, tx)
+
+            elif "" not in (sx, ax, tx):
+                vx = functions.v_sat(ax, ax, tx)
+
+            elif "" not in (sx, ux, ax):
+                vx = functions.v_sua(sx, ux, ax)
+
+            elif "" not in (sx, ux, tx):
+                vx = functions.v_sut(sx, ux, tx)
+
+            else: conditions_unmet += 1
+
+        if vy == "":
+
+            if "" not in (uy, ay, ty):
+                vy = functions.v_uat(uy, ay, ty)
+
+            elif "" not in (sy, ay, ty):
+                vy = functions.v_sat(ay, ay, ty)
+
+            elif "" not in (sy, uy, ay):
+                vy = functions.v_sua(sy, uy, ay)
+
+            elif "" not in (sy, uy, ty):
+                vy = functions.v_sut(sy, uy, ty)
+
+            else: conditions_unmet += 1
+
+        if ax == "":
+
+            if "" not in (ux, vx, tx):
+                ax = functions.a_uvt(ux, vx, tx)
+
+            elif "" not in (sx, ux, tx):
+                ax = functions.a_sut(sx, ux, tx)
+
+            elif "" not in (sx, vx, tx):
+                ax = functions.a_svt(sx, vx, tx)
+
+            elif "" not in (sx, ux, vx):
+                ax = functions.a_suv(sx, ux, vx)
 
             else: conditions_unmet += 1
 
         if ay == "":
-            if "" not in (uy, sy):
-                ay = functions.ay_equals_minus_uy_sq_over_2sy(uy, sy)
 
-            elif "" not in (uy, ty):
-                ay = functions.ay_equals_minus_uy_over_ty(uy, ty)
+            if "" not in (uy, vy, ty):
+                ay = functions.a_uvt(uy, vy, ty)
 
             elif "" not in (sy, uy, ty):
-                ay = functions.ay_equals_2_times_sy_minus_uyty_over_ty_sq(sy, uy, ty)
+                ay = functions.a_sut(sy, uy, ty)
 
-            elif "" not in (sy, ty):
-                ay = functions.ay_equals_minus_2_times_sy_minus_vyty_over_ty_sq(sy, ty)
+            elif "" not in (sy, vy, ty):
+                ay = functions.a_svt(sy, vy, ty)
+
+            elif "" not in (sy, uy, vy):
+                ay = functions.a_suv(sy, uy, vy)
+
+            else: conditions_unmet += 1
+
+        if tx == "":
+
+            if "" not in (sx, ux, vx):
+                tx = functions.t_suv(sx, ux, vx)
+
+            elif "" not in (ux, vx, ax):
+                tx = functions.t_uva(ux, vx, ax)
+
+            elif "" not in (sx, vx, ax):
+                tx = functions.t_sva(sx, vx, ax)
+
+            elif "" not in (sx, ux, ax):
+                tx = functions.t_sua(sx, ux, ax)
+
+            else: conditions_unmet += 1
+
+        if ty == "":
+
+            if "" not in (sy, uy, vy):
+                ty = functions.t_suv(sy, uy, vy)
+
+            elif "" not in (uy, vy, ay):
+                ty = functions.t_uva(uy, vy, ay)
+
+            elif "" not in (sy, vy, ay):
+                ty = functions.t_sva(sy, vy, ay)
+
+            elif "" not in (sy, uy, ay):
+                ty = functions.t_sua(sy, uy, ay)
 
             else: conditions_unmet += 1
 
@@ -136,8 +221,8 @@ def menuvalidator(sx, ux, vx, tx, sy, uy, ay, ty):
             print("-"*116)
 
             return True
-        elif conditions_unmet == last_conditions_unmet:
-            return False
+
+        elif conditions_unmet == last_conditions_unmet: return False
 
         else:
             last_conditions_unmet = conditions_unmet
